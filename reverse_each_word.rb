9a3970d068473632e37226a8fc69024138868259
chris brady <chrisb3546@yahoo.com>
2020-03-28 19:12:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ sentence1 = "Hello there, and how are you?"
 
 def reverse_each_word(sentence1)
   a= sentence1.split
-  b = a.map do |word|
+  b = a.collect do |word|
    word.reverse
    end
    b.join(" ")
