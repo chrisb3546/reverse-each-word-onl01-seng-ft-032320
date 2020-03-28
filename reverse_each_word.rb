8dbcd88d6d432ab12a19d28331d875fc31e0f4c7
chris brady <chrisb3546@yahoo.com>
2020-03-28 18:26:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
-def reverse_each_word(sentence1)
- a = sentence1.split
- a.each do |sentence1|
+
+def reverse_test(sentence1)
+  a= sentence1.split
+   a.map do |sentence1|
    puts "#{sentence1}".reverse
   end
+  
 end
 
-reverse_each_word(sentence1)
+reverse_test(sentence1)
